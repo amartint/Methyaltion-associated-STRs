521d953c1ba2fb57837db44796cd65760a360db4
Alexmssm <77851639+amartint@users.noreply.github.com>
2022-10-20 16:04:33 -0400

    Extract HipSTR derived genotypes from vcf
    Extract HipSTR derived genotypes from vcf

---
+++ scripts/STRgenotypes/ExtractingFilteredSTRGenotypesFromVCF.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+while getopts d:i: flag
+do
+    case "${flag}" in
+        d) directory=${OPTARG};;
+        i) input=${OPTARG};;
+    esac
+done
+echo "Directory: $directory"; echo "Input: $input";
+
+
+echo "Extractintg genotypes from $INPUT.vcf.gz "
+
+echo "WD: $Dir"
+cd $Dir
+        
+prefix=`basename $INPUT | sed -e 's/.vcf.gz$//g' -e 's/.vcf//g'`        
+bcftools query -f '[%CHROM\t%START\t%END\t%POS\t%ID\t%PERIOD\t%AN\t%SAMPLE\t%GT\t%REF\t%ALT\n]' $INPUT.filtered.vcf.gz | awk '$9!="./."'| tr "|" "\t" |
+awk 'BEGIN{ FS = "\t"; OFS = "\t"}{
+        str = $11","$12;
+        split(str,arr,",");
+        $9=length(arr[$9+1])/$6;
+        $10=length(arr[$10+1])/$6;
+        print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10}' > $INPUT.filtered.txt
+        
+
+echo "The end"
