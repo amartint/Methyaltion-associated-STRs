0cde0fe40be3b907997f1f37e0cf71d99e6a3bf1
Alexmssm <77851639+amartint@users.noreply.github.com>
2022-12-14 09:45:14 -0500

    Script for performing QC on DNA methylation data

--- scripts/QualityControl.DNAmetylation.r
+++ scripts/QualityControl.DNAmetylation.r
@@ -1,20 +1,18 @@
-
 cat (paste0("\n", Sys.Date(),"\n"))
-cat(paste0("R version: ",getRversion()),"\n")
+cat (paste0("R version: ",getRversion(),"\n"))
 
 
 ##########################################################################################################################
-# Loading require libraries										 			#
+# Loading require libraries										 			
 ##########################################################################################################################
 
 cat (paste0("Loading libraries\n"))
-suppressPackageStartupMessages(library (dplyr))
-suppressPackageStartupMessages(library (data.table))
-suppressPackageStartupMessages(library (tidyverse))
-suppressPackageStartupMessages(library (ggplot2))
-suppressPackageStartupMessages(library (scales))
-suppressPackageStartupMessages(library (R.utils))
-suppressPackageStartupMessages(library (argparse))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(data.table))
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(scales))
+suppressPackageStartupMessages(library(argparse))
 
 
 ##########################################################################################################################
@@ -23,16 +21,15 @@ suppressPackageStartupMessages(library (argparse))
 
 # create parser object and specify our desired options 
 parser <- ArgumentParser()
-parser$add_argument("-d", "--directory", help="add  working directory")
+parser$add_argument("-d", "--directory", help="working directory")
 parser$add_argument("-i", "--input", help="input file containing methylation data (columns: ProbeID, sample1, sample2, ... sample n)") 
 args <- parser$parse_args()
 if (length (args)==0) { stop("please provide arguments!!")}
 
   
 cat (paste0("Arguments:\n"))
-mydir=args$directory; cat(" \tWorking directory): ", args$directory, "\n")
-myfile=args$input; cat(" \tFilename: ", args$input, "\n")
-
+MYDIR=args$directory; cat(" \tWorking directory): ", MYDIR, "\n")
+myfile=args$input; cat(" \tFilename: ", myfile, "\n")
 MYDIROUT= paste0(MYDIR, "/qc"); if (!file.exists(MYDIROUT)) { dir.create(MYDIROUT)}
 mytheme <- theme_bw () +
         		theme(plot.title = element_text(face="bold",size = 22,colour = "black"),
@@ -58,29 +55,29 @@ mytheme <- theme_bw () +
 
 densityplot <- function (MYFILE) {
 	
-
-	cat (paste0(" ... calculating density copy number across regions on ", MYFILE),"\n")
-	n <- length(unique(df$sampleID));
-	Nregions <- format(length (unique(df$geneNames)),big.mark=",")
-	plotname <- paste0(MYDIROUT, "/",gsub(".txt","", MYFILE),".qc_densityplot.n",n,".png")
-	txtname  <- paste0(MYDIROUT, "/",gsub(".txt","", MYFILE),".qc_densityplot.n",n,".txt")
+	cat (paste0(" ... calculating density copy number across regions on ", myfile,"\n"))
+	n <- ncol(MYFILE) -1;
+	Nregions <- format(length (unique(MYFILE$ProbeID)),big.mark=",")
+	plotname <- paste0(MYDIROUT, "/",gsub(".txt","", myfile),".qc_densityplot.n",n,".png")
+	txtname  <- paste0(MYDIROUT, "/",gsub(".txt","", myfile),".qc_densityplot.n",n,".txt")
 	YLAB="Density"; 
 	XLAB="ylab"
 	TITLE=paste0("Mytitle")
 	SUBTITLE=paste0("regions: ",Nregions , " across ", n," samples")
 	
 	# get density peak per sample
-	x <- melt (MYFILE)
-	labels <- x %>% group_by (sampleID) %>% summarise( 
-					yPos=max(density(CN)$y), yPos = round (yPos,2)) %>% 
+	x <- melt (MYFILE) %>% rename (sampleId = variable) %>% filter (!is.na (value)) %>% as.data.frame
+	labels <- x %>% group_by (sampleId) %>% summarise( 
+					yPos=max(density(value)$y), 
+					yPos = round (yPos,2)) %>% 
 				ungroup %>% data.frame	%>%
 				arrange (desc(yPos)) 
 	cat (paste0(" ... saving ... \n"))
 	write.table (labels,txtname, sep ="\t", row.names =F, quote =F)
 	
 	cat (paste0(" ... plotting ... \n"))
-	g <- ggplot (df)  +  
-		geom_density (aes(x = value, color= sampleID ), show.legend = FALSE)  +  
+	g <- ggplot (x)  +  
+		geom_density (aes(x = value, color= sampleId ), show.legend = FALSE)  +  
 		labs (xlab = XLAB, ylab = YLAB, title = TITLE, subtitle = SUBTITLE) + 
 		scale_y_continuous(breaks = pretty_breaks ()) +
 		scale_x_continuous(breaks = pretty_breaks ()) + 
@@ -93,39 +90,37 @@ densityplot <- function (MYFILE) {
 }	
 pca <- function(MYFILE) {
 	
-	setwd (MYDIR)
-	MYDIROUT= paste0(MYDIR, "/qc"); if (!file.exists(MYDIROUT)) { dir.create(MYDIROUT)}
-
-	cat (paste0(" ... calculating pc across regions on ", mychrom,"\n"))
-	Nregions=format (length(unique(myfile$geneNames)), big.mark = ",")
-	n <- length(unique(myfile$sampleID))
-	plotname <- paste0(MYDIROUT, "/",gsub(".txt","", MYFILE),".qc_pca.n",n,".png")
-	txtname  <- paste0(MYDIROUT, "/",gsub(".txt","", MYFILE),".qc_pca.n",n,".txt")
-	varname  <- paste0(MYDIROUT, "/",gsub(".txt","", MYFILE),".qc_pca_variance.n",n,".txt")
+	cat (paste0(" ... calculating pca across regions on ", myfile,"\n"))
+	Nregions=format (length(unique(MYFILE$ProbeID)), big.mark = ",")
+	n <- ncol(MYFILE) -1;
+	plotname <- paste0(MYDIROUT, "/",gsub(".txt","", myfile),".qc_pca.n",n,".png")
+	txtname  <- paste0(MYDIROUT, "/",gsub(".txt","", myfile),".qc_pca.n",n,".txt")
+	varname  <- paste0(MYDIROUT, "/",gsub(".txt","", myfile),".qc_pca_variance.n",n,".txt")
 	
-	rownames(myfile) = myfile[,1]; myfile = myfile[,-1];
-	myfile <- na.omit(myfile) 
-	myfile <- t(myfile)
+	rownames(MYFILE) = MYFILE[,1]; 
+	temp <- MYFILE[,-1];
+	temp <- na.omit(temp) 
+	temp  <- t(temp )
 	
 	cat ("\t\t ... comuputing pcs ... \n")
- 	res.pca <- prcomp(myfile, scale = FALSE)  
+ 	res.pca <- prcomp(temp, scale = FALSE)  
      
  	# get principal components
  	cat ("\t\t ... extracting pcs ... \n")
  	pca = res.pca$x %>% data.frame
- 	pca = cbind(sampleID = rownames(pca), round(pca,4)) %>%select (1:50)
+ 	pca = cbind(sampleID = rownames(pca), round(pca,4)) %>% select (1:50)
 	
  	cat ("\t\t ... getting variance explained by each PC... \n") # get and calculate proportion of variance for component
  	std_dev <- res.pca$sdev; rm (res.pca)
  	pr_var <- std_dev^2
- 	prop_varex <- round((pr_var/sum(pr_var) *100 ),1)
+ 	prop_varex <- round((pr_var/sum(pr_var) *100 ),2)
  	a <- paste0("PC",1:length (prop_varex ));
  	prop_varex <- as.data.frame(cbind("PCs"=a,prop_varex))
  	write.table (prop_varex,varname , sep ="\t", row.names = F,quote = F) 
  	prop_varex = read.table(varname , sep ="\t", check.names = F,header =T)
  	
  	TITLE=paste0("PCA")
- 	SUBTITLE=paste0("Regions:  ", format(Nregions,big.mark=","), " regions across ",format ( Nsamples,big.mark = ","," samples")
+ 	SUBTITLE=paste0("Regions:  ", format(Nregions,big.mark=","), " regions across ",format (n,big.mark = ",")," samples")
  	
  	pdf ( plotname , width = 8, height = 8)
  	for (j in 1:9) {
@@ -133,9 +128,9 @@ pca <- function(MYFILE) {
           	B <- pca [,grep (paste0("^PC",j+1,"$"),colnames(pca))]
           	XLAB = paste0("PC",j," (",prop_varex[j,2],"%)")
           	YLAB = paste0("PC",j+1," (",prop_varex[j+1,2],"%)")
-          	p<- ggplot (pca,aes(x=A,y=B))+  geom_point (size=2,alpha = 0.9) + 
-          	  + mytheme +
-          	labs = (xlab = XLAB, ylab = YLAB , title = TITLE, subtitle = SUBTITLE)
+          	p<- ggplot (pca,aes(x=A,y=B)) +  geom_point (size=2,alpha = 0.9) + 
+          	 	mytheme +
+          	labs (xlab = XLAB, ylab = YLAB , title = TITLE, subtitle = SUBTITLE)
      	 	print (p)
      	 	rm (A,B, XLAB, YLAB,p)
  	}
@@ -161,10 +156,10 @@ pca <- function(MYFILE) {
 #	loading file	and run custom functions								 		
 ##########################################################################################################################
 
-
+setwd(MYDIR)
 cat (paste0("... loading eth data: ", MYFILE,"\n"))
-df <- fread (myfile, sep = "\t", check.names =F, header = T)
-densityplot (df )
+df <- fread (myfile, sep = "\t", check.names =F, header = T) %>% as.data.frame
+densityplot (df)
 pca (df)
 
 cat ("The end")
