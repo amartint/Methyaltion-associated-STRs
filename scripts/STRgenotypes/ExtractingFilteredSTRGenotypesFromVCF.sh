2f6b57d1c2b29716a32c410e679308747d399a2c
Alexmssm <77851639+amartint@users.noreply.github.com>
2022-12-14 09:41:39 -0500

    Script for extracting STR genotypes from VCF file

--- scripts/STRgenotypes/ExtractingFilteredSTRGenotypesFromVCF.sh
+++ scripts/STRgenotypes/ExtractingFilteredSTRGenotypesFromVCF.sh
@@ -11,19 +11,16 @@ done
 echo "Directory: $directory"; echo "Input: $input";
 
 
-echo "Extractintg genotypes from $INPUT.vcf.gz "
-
-echo "WD: $Dir"
-cd $Dir
-        
-prefix=`basename $INPUT | sed -e 's/.vcf.gz$//g' -e 's/.vcf//g'`        
-bcftools query -f '[%CHROM\t%START\t%END\t%POS\t%ID\t%PERIOD\t%AN\t%SAMPLE\t%GT\t%REF\t%ALT\n]' $INPUT.filtered.vcf.gz | awk '$9!="./."'| tr "|" "\t" |
-awk 'BEGIN{ FS = "\t"; OFS = "\t"}{
+echo "Extractintg genotypes from $input.filtered.vcf.gz"
+cd $directory
+bcftools query -f '[%CHROM\t%START\t%END\t%POS\t%ID\t%PERIOD\t%AN\t%SAMPLE\t%GT\t%REF\t%ALT\n]' $input.filtered.vcf.gz | awk '$9!="./."'| tr "|" "\t" |\
+	awk 'BEGIN{ FS = "\t"; OFS = "\t"}{
         str = $11","$12;
         split(str,arr,",");
         $9=length(arr[$9+1])/$6;
         $10=length(arr[$10+1])/$6;
-        print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10}' > $INPUT.filtered.txt
+        print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10}' > $input.filtered.txt
         
 
 echo "The end"
+
